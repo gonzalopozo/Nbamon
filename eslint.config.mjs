dcefa032abeee471e1bbdf3069dd50c843cd3167
gonzalopozo <gonzalopozo2005@gmail.com>
2026-03-02 12:14:40 +0100

    fix & style: bug fight & format code with Prettier with one tab

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,31 +2,32 @@ import globals from "globals";
 import js from "@eslint/js";
 
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
-      },
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            indent: ["error", 4, { SwitchCase: 1 }],
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+        },
     },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "error",
+    {
+        files: ["public/**/*.js"],
+        languageOptions: {
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+            },
+        },
     },
-  },
-  {
-    files: ["public/**/*.js"],
-    languageOptions: {
-      sourceType: "script",
-      globals: {
-        ...globals.browser,
-      },
+    {
+        ignores: ["node_modules/"],
     },
-  },
-  {
-    ignores: ["node_modules/"],
-  },
 ];
--- index.js
+++ index.js
@@ -11,98 +11,98 @@ app.use(express.json());
 const jugadores = [];
 
 class Jugador {
-  constructor(id) {
-    this.id = id;
-  }
+    constructor(id) {
+        this.id = id;
+    }
 
-  asignarNbamon(nbamon) {
-    this.nbamon = nbamon;
-  }
+    asignarNbamon(nbamon) {
+        this.nbamon = nbamon;
+    }
 
-  actualizarPosicion(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+    actualizarPosicion(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  asignarAtaques(ataques) {
-    this.ataques = ataques;
-  }
+    asignarAtaques(ataques) {
+        this.ataques = ataques;
+    }
 }
 
 class Nbamon {
-  constructor(nombre) {
-    this.nombre = nombre;
-  }
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
 }
 
 app.get("/unirse", (req, res) => {
-  const id = `${Math.random()}`;
+    const id = `${Math.random()}`;
 
-  const jugador = new Jugador(id);
+    const jugador = new Jugador(id);
 
-  jugadores.push(jugador);
+    jugadores.push(jugador);
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.send(id);
+    res.send(id);
 });
 
 app.post("/nbamon/:jugadorId", (req, res) => {
-  const jugadorId = req.params.jugadorId || "";
-  const nombre = req.body.nbamon || "";
-  const nbamon = new Nbamon(nombre);
+    const jugadorId = req.params.jugadorId || "";
+    const nombre = req.body.nbamon || "";
+    const nbamon = new Nbamon(nombre);
 
-  const jugadorIndex = jugadores.findIndex(
-    (jugador) => jugadorId === jugador.id,
-  );
+    const jugadorIndex = jugadores.findIndex(
+        (jugador) => jugadorId === jugador.id,
+    );
 
-  if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].asignarNbamon(nbamon);
-  }
-  res.end();
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarNbamon(nbamon);
+    }
+    res.end();
 });
 
 app.post("/nbamon/:jugadorId/posicion", (req, res) => {
-  const jugadorId = req.params.jugadorId || "";
-  const x = req.body.x || 0;
-  const y = req.body.y || 0;
+    const jugadorId = req.params.jugadorId || "";
+    const x = req.body.x || 0;
+    const y = req.body.y || 0;
 
-  const jugadorIndex = jugadores.findIndex(
-    (jugador) => jugadorId === jugador.id,
-  );
+    const jugadorIndex = jugadores.findIndex(
+        (jugador) => jugadorId === jugador.id,
+    );
 
-  if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].actualizarPosicion(x, y);
-  }
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].actualizarPosicion(x, y);
+    }
 
-  const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id);
+    const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id);
 
-  res.send({
-    enemigos,
-  });
+    res.send({
+        enemigos,
+    });
 });
 
 app.post("/nbamon/:jugadorId/ataques", (req, res) => {
-  const jugadorId = req.params.jugadorId || "";
-  const ataques = req.body.ataques || [];
+    const jugadorId = req.params.jugadorId || "";
+    const ataques = req.body.ataques || [];
 
-  const jugadorIndex = jugadores.findIndex(
-    (jugador) => jugadorId === jugador.id,
-  );
+    const jugadorIndex = jugadores.findIndex(
+        (jugador) => jugadorId === jugador.id,
+    );
 
-  if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].asignarAtaques(ataques);
-  }
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarAtaques(ataques);
+    }
 
-  res.end();
+    res.end();
 });
 
 app.get("/nbamon/:jugadorId/ataques", (req, res) => {
-  const jugadorId = req.params.jugadorId || "";
-  const jugador = jugadores.find((jugador) => jugador.id === jugadorId);
-  res.send({
-    ataques: jugador.ataques || [],
-  });
+    const jugadorId = req.params.jugadorId || "";
+    const jugador = jugadores.find((jugador) => jugador.id === jugadorId);
+    res.send({
+        ataques: jugador.ataques || [],
+    });
 });
 
 app.listen(port, "0.0.0.0");
