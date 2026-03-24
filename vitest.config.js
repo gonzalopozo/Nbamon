742763399f163881406b286eebbb3f3b784ff6c5
gonzalopozo <gonzalopozo2005@gmail.com>
2026-03-24 11:48:03 +0100

    fix: vitest wrong config used to run every node_modules and git random tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         environment: "node",
-        exclude: ["./e2e/**"],
+        exclude: [...configDefaults.exclude, "e2e/**"],
     },
 });
